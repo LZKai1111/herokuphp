4e90a6c22ecd7778ce218a372bf756052b873cfc
LZKai1111 <20014063@myrp.edu.sg>
2022-07-04 12:39:37 +0800

    meta description added.

--- web/test.php
+++ web/test.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html lang="en">
 <head>
     <title>Hi</title>
-    <meta name="description">
+    <meta name="description">This is a test</meta>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
