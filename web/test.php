65c1490bccdd7987cbee185908c12056c075ff91
LZKai1111 <20014063@myrp.edu.sg>
2022-07-04 12:36:59 +0800

    test.php more attributes added

--- web/test.php
+++ web/test.php
@@ -1,9 +1,10 @@
-<html>
+<!DOCTYPE html lang="en">
 <head>
     <title>Hi</title>
+    <meta name="description">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
   Hello World!
 </body>
-</html>
+</!DOCTYPE>
