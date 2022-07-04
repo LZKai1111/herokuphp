83895d2d026d3fe8309c7122c15e9bff8d17e8be
LZKai1111 <20014063@myrp.edu.sg>
2022-07-04 12:53:49 +0800

    meta changes

--- web/test.php
+++ web/test.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <title>Hi</title>
-    <meta name="description">This is a test</meta>
-    <meta name="viewport"></meta>
+    <meta name="description" content="This is a test"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
 </head>
 <body>
   Hello World! burger
