ef3f91ea2f8f8dec13c44f86d273478f0f649218
LZKai1111 <20014063@myrp.edu.sg>
2022-07-04 12:45:23 +0800

    test

--- web/test.php
+++ web/test.php
@@ -2,9 +2,9 @@
 <head>
     <title>Hi</title>
     <meta name="description">This is a test</meta>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 </head>
 <body>
-  Hello World!
+  Hello World! burger
 </body>
 </!DOCTYPE>
